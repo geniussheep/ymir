9967ba53a20979b9c1fbc8ac3fd4d1d387f7d69e
杨杨 <geniussheep@hotmail.com>
2021-11-15 20:18:45 +0800

    调整日志

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestLogger(t *testing.T) {
-	l := zap.NewLogger(WithLevel(TraceLevel), WithName("zap"))
+	l, err := zap.NewLogger(WithLevel(TraceLevel), WithName("zap"))
+	if err != nil {
+
+	}
 	h1 := NewHelper(l).WithFields(map[string]interface{}{"key1": "val1"})
 	h1.Trace("trace_msg1")
 	h1.Warn("warn_msg1")
