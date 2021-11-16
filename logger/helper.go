e5123cdb7092a2723f269c62b661a8af0bf83cb3
杨杨 <geniussheep@hotmail.com>
2021-11-16 20:04:17 +0800

    调整日志--新增helper内相关logw的扩展方法

--- logger/helper.go
+++ logger/helper.go
@@ -14,88 +14,77 @@ func NewHelper(log Logger) *Helper {
 }
 
 func (h *Helper) Info(args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(InfoLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Log(InfoLevel, args...)
+	h.Log(InfoLevel, args)
 }
 
-func (h *Helper) Infof(template string, args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(InfoLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Logf(InfoLevel, template, args...)
+func (h *Helper) Infof(format string, args ...interface{}) {
+	h.Logf(InfoLevel, format, args...)
+}
+
+func (h *Helper) Infow(fields map[string]interface{}, format string, args ...interface{}) {
+	h.Logw(InfoLevel, fields, format, args...)
 }
 
 func (h *Helper) Trace(args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(TraceLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Log(TraceLevel, args...)
+	h.Log(TraceLevel, args...)
 }
 
-func (h *Helper) Tracef(template string, args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(TraceLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Logf(TraceLevel, template, args...)
+func (h *Helper) Tracef(format string, args ...interface{}) {
+	h.Logf(TraceLevel, format, args...)
+}
+
+func (h *Helper) Tracew(fields map[string]interface{}, format string, args ...interface{}) {
+	h.Logw(TraceLevel, fields, format, args...)
 }
 
 func (h *Helper) Debug(args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(DebugLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Log(DebugLevel, args...)
+	h.Log(DebugLevel, args...)
 }
 
-func (h *Helper) Debugf(template string, args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(DebugLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Logf(DebugLevel, template, args...)
+func (h *Helper) Debugf(format string, args ...interface{}) {
+	h.Logf(DebugLevel, format, args...)
+}
+
+func (h *Helper) Debugw(fields map[string]interface{}, format string, args ...interface{}) {
+	h.Logw(DebugLevel, fields, format, args...)
 }
 
 func (h *Helper) Warn(args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(WarnLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Log(WarnLevel, args...)
+	h.Log(WarnLevel, args...)
 }
 
-func (h *Helper) Warnf(template string, args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(WarnLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Logf(WarnLevel, template, args...)
+func (h *Helper) Warnf(format string, args ...interface{}) {
+	h.Logf(WarnLevel, format, args...)
+}
+
+func (h *Helper) Warnw(fields map[string]interface{}, format string, args ...interface{}) {
+	h.Logw(WarnLevel, fields, format, args...)
 }
 
 func (h *Helper) Error(args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(ErrorLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Log(ErrorLevel, args...)
+	h.Log(ErrorLevel, args...)
 }
 
-func (h *Helper) Errorf(template string, args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(ErrorLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Logf(ErrorLevel, template, args...)
+func (h *Helper) Errorf(format string, args ...interface{}) {
+	h.Logf(ErrorLevel, format, args...)
+}
+
+func (h *Helper) Errorw(fields map[string]interface{}, format string, args ...interface{}) {
+	h.Logw(ErrorLevel, fields, format, args...)
 }
 
 func (h *Helper) Fatal(args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(FatalLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Log(FatalLevel, args...)
+	h.Log(FatalLevel, args...)
 	os.Exit(1)
 }
 
-func (h *Helper) Fatalf(template string, args ...interface{}) {
-	if !h.Logger.Options().Level.Enabled(FatalLevel) {
-		return
-	}
-	h.Logger.Fields(h.fields).Logf(FatalLevel, template, args...)
+func (h *Helper) Fatalf(format string, args ...interface{}) {
+	h.Logf(FatalLevel, format, args...)
+	os.Exit(1)
+}
+
+func (h *Helper) Fatalw(fields map[string]interface{}, format string, args ...interface{}) {
+	h.Logw(FatalLevel, fields, format, args...)
 	os.Exit(1)
 }
 
@@ -111,4 +100,26 @@ func (h *Helper) WithFields(fields map[string]interface{}) *Helper {
 		nfields[k] = v
 	}
 	return &Helper{Logger: h.Logger, fields: nfields}
+}
+
+func (h *Helper) Log(level Level, args ...interface{}) {
+	if !h.Logger.Options().Level.Enabled(level) {
+		return
+	}
+	h.Logger.Fields(h.fields).Log(level, args...)
+}
+
+func (h *Helper) Logf(level Level, format string, v ...interface{}) {
+	if !h.Logger.Options().Level.Enabled(level) {
+		return
+	}
+	h.Logger.Fields(h.fields).Logf(level, format, v...)
+}
+
+// Logf writes a msg log entry with some custom field
+func (h *Helper) Logw(level Level, fileds map[string]interface{}, format string, v ...interface{}) {
+	if !h.Logger.Options().Level.Enabled(level) {
+		return
+	}
+	h.Logger.Fields(h.fields).Logw(level, fileds, format, v...)
 }
