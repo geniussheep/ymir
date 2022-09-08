ffa18ecd8801a855fe66b7881b02ec5729995c78
杨杨 <geniussheep@hotmail.com>
2022-09-08 17:30:29 +0800

    调整db逻辑新增dbBool类型处理bool值的转换

--- storage/db/db_test.go
+++ storage/db/db_test.go
@@ -4,6 +4,7 @@ import (
 	"gitlab.benlai.work/go/ymir/logger"
 	dbLogger "gorm.io/gorm/logger"
 	"testing"
+	"time"
 )
 
 func TestDriver(t *testing.T) {
@@ -90,6 +91,84 @@ func TestDb(t *testing.T) {
 	err = yorm.UpdateBatch(updateFileds, where, &model)
 
 	err = yorm.FindByQuery(where, &model)
+
+	logger.Infof("get model %v", model)
+}
+
+type ApplicationDomainServer struct {
+
+	// 应用domain Id
+	Id int `json:"id" gorm:"column:id;primaryKey;autoIncrement;comment:domain id"`
+
+	// 服务端应用id
+	ServerAppId int `json:"server_app_Id" gorm:"column:server_app_Id;size:256;comment:应用id"`
+
+	// 应用Domain名称
+	DomainName string `json:"domain_name" gorm:"column:domain_name;size:256;comment:应用Domain名称"`
+
+	// 应用Domain地址
+	DomainAddress string `json:"domain_address" gorm:"column:domain_address;size:256;comment:应用Domain地址"`
+
+	// 应用状态 -- 禁用:disabled, 启用:ready
+	Status string `json:"status" gorm:"column:status;size:32;comment:状态"`
+
+	// 服务版本号
+	Version string `json:"version" gorm:"column:version;size:46;comment:服务版本号"`
+
+	// 是否是公共服务
+	IsPublic DbBool `json:"isPublic" gorm:"column:isPublic;comment:是否是公共服务"`
+
+	// 唯一Id
+	SId int `json:"sid" gorm:"column:sid;comment:唯一id"`
+
+	// 是否已删除
+	IsDel int `json:"is_del" gorm:"column:is_del;comment:是否已删除"`
+
+	// 创建时间
+	CreatedTime *time.Time `gorm:"column:created_time" json:"created_time"`
+
+	// 更新时间
+	UpdatedTime *time.Time `gorm:"column:updated_time" json:"updated_time"`
+
+	//应用名称
+	AppName string `json:"appName"`
+
+	//应用别名
+	AppAliasName string `json:"appAliasName"`
+}
+
+func (a *ApplicationDomainServer) Validate() bool {
+	return a.Id > 0
+}
+
+func (ApplicationDomainServer) TableName() string {
+	return "tb_esb_rpc_application_domain_servers"
+}
+
+func TestDbmsDb(t *testing.T) {
+	var model ApplicationDomainServer
+	l := logger.NewLogger(logger.WithLevel(logger.DebugLevel))
+	logger.DefaultLogger = l
+	l.Log(logger.InfoLevel, "test-logger")
+	yorm, err := New(
+		SetDsn("root:bb.123#lm.com@tcp(10.250.240.59:3306)/BenlaiEsbBrancheNew?charset=utf8&parseTime=true"),
+		SetDriver("mysql"),
+		//SetUseDbms(true),
+		SetLogLevel(dbLogger.LogLevel(logger.DebugLevel.LevelForGorm())))
+	if err != nil {
+		logger.Errorf("Service.Esb Get error:%e", err.Error())
+	}
+
+	where := []interface{}{
+		[]interface{}{"server_app_id", "in", []int{5031, 8338}},
+		[]interface{}{"is_del", "=", false},
+	}
+
+	err = yorm.FindByQuery(where, &model)
+
+	if err != nil {
+		logger.Errorf("Service.Esb Get error:%s", err.Error())
+	}
 
 	logger.Infof("get model %v", model)
 }
