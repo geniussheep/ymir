66d3d410e5852797c00b627901c48a58dfaa7478
yangyang1 <yangyang1@benlai.com>
2023-03-08 17:21:15 +0800

    新增MapToModel、MaptoJson扩展方法

--- sdk/pkg/extend.go
+++ sdk/pkg/extend.go
@@ -1,5 +1,7 @@
 package pkg
 
+import "encoding/json"
+
 func If[T any](condition bool, trueVal, falseVal T) T {
 	if condition {
 		return trueVal
@@ -14,4 +16,18 @@ func Contains[T comparable](s []T, e T) bool {
 		}
 	}
 	return false
+}
+
+func MapToModel[T interface{}](jsonMap map[string]string) T {
+	js, _ := json.Marshal(jsonMap)
+	var s T
+	json.Unmarshal(js, &s)
+	return s
+}
+
+func MaptoJson[T interface{}](t T) map[string]string {
+	js, _ := json.Marshal(t)
+	s := map[string]string{}
+	json.Unmarshal(js, &s)
+	return s
 }
